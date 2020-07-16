53cc00743a95ad72a2c4fb0b681072412ac7ad98
Umar Farooq <umar19960607@gmail.com>
2020-07-15 23:35:37 -0400

    Adding game history

---
+++ src/square.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+export function Square(props) {
+    return (
+        <button className="square"
+                onClick={() => {
+                    props.onClick();
+                }}
+        >
+            {props.value}
+        </button>
+    );
+}
